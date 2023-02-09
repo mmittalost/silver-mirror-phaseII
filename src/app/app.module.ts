9e02f820853a27f0c7f9a801fd3b0baf842f4e56
manish <manish@ost.agency>
2023-02-09 18:34:29 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookAnAppointmentComponent } from './book-an-appointment/book-an-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookAnAppointmentComponent
   ],
   imports: [
     BrowserModule,
